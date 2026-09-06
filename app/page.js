048db4a9aa056c79804f5b4010f80b1d6315ad23
Chuchuayy <chuchuiuay@gmail.com>
2026-09-06 13:56:39 +0700

    add homepage

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>DA News</h1>
+      <p>Asia news, in English, for everyone.</p>
+    </main>
+  );
+    }
